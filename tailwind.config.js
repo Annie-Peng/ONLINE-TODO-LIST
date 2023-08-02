a1d02737cb504dea8dcd149920b43826d868cd43
Annie-Peng <djs910532@gmail.com>
2023-08-02 09:59:41 +0800

    add tailwind & adjust css setting

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      primary: "#FFD370",
+      secondary: "#333333",
+      tertiary: "#9F9A91",
+      warning: "#D87355",
+      white: "#FFFFFF",
+    },
+    fontFamily: {
+      sans: ["Noto Sans TC"],
+    },
+  },
+  plugins: [],
+};
