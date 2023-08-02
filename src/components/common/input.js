c64dc24fa385a8274a291cedfd0a994205065e35
Annie-Peng <djs910532@gmail.com>
2023-08-02 23:34:39 +0800

    add transfer between login and register

--- src/components/common/input.js
+++ src/components/common/input.js
@@ -1,7 +1,8 @@
-export default function Input({ value, showContent, onChange }) {
+export default function Input({ name, value, showContent, onChange }) {
   return (
     <input
       className="peer mt-1 py-3 px-4 rounded-[10px] placeholder:text-tertiary text-medium w-full"
+      name={name}
       value={value}
       placeholder={`請輸入${showContent}`}
       onChange={onChange}
--- src/components/login/formContent.js
+++ src/components/login/formContent.js
@@ -2,29 +2,43 @@ import { useState } from "react";
 import MainBtn from "../common/btn.js";
 import Input from "../common/input.js";
 
-function FormInput({ content, value, onChange }) {
+function FormInput({ name, content, value, onChange }) {
   return (
     <label>
       <p className="mt-4 text-sm font-bold">{content}</p>
-      <Input value={value} showContent={content} onChange={onChange} />
+      <Input
+        name={name}
+        value={value}
+        showContent={content}
+        onChange={onChange}
+      />
     </label>
   );
 }
 
-function Login({ isLogin }) {
-  if (!isLogin) return null;
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+function Login({ showLogin, onClickToRegister }) {
+  const [cusInfo, setCusInfo] = useState({
+    email: "",
+    password: "",
+  });
   const [emailStatus, setEmailStatus] = useState("typing");
   const [passwordStatus, setPasswordStatus] = useState("typing");
+
+  if (!showLogin) return null;
 
   const isEmptyEmail = emailStatus === null;
   const isEmptyPassword = passwordStatus === null;
 
   function checkForm() {
-    setEmailStatus(email ? "typing" : null);
-    setPasswordStatus(password ? "typing" : null);
+    setEmailStatus(cusInfo.email ? "typing" : null);
+    setPasswordStatus(cusInfo.password ? "typing" : null);
+  }
+
+  function handleChange(e) {
+    setCusInfo({
+      ...cusInfo,
+      [e.target.name]: e.target.value,
+    });
   }
 
   return (
@@ -33,16 +47,18 @@ function Login({ isLogin }) {
       <form className="mt-2">
         <FormInput
           content="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+          value={cusInfo.email}
+          onChange={handleChange}
         />
         {isEmptyEmail && (
           <p className="text-warning text-sm font-bold">此欄位不可為空</p>
         )}
         <FormInput
           content="密碼"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          name="password"
+          value={cusInfo.password}
+          onChange={handleChange}
         />
         {isEmptyPassword && (
           <p className="text-warning text-sm font-bold">此欄位不可為空</p>
@@ -51,7 +67,7 @@ function Login({ isLogin }) {
       <MainBtn value={"登入"} onClick={checkForm} />
       <button
         className="block mx-auto mt-6 font-bold"
-        // onClickTransferPage={onClick}
+        onClick={onClickToRegister}
       >
         註冊帳號
       </button>
@@ -59,21 +75,107 @@ function Login({ isLogin }) {
   );
 }
 
-function Register() {}
+function Register({ showRegister, onClickToLogin }) {
+  const [emptyStatus, setEmptyStatus] = useState({
+    email: "",
+    name: "",
+    password: "",
+    rePassword: "",
+  });
+  const [cusInfo, setCusInfo] = useState({
+    email: "",
+    name: "",
+    password: "",
+    rePassword: "",
+  });
 
-export default function FormContent() {
-  const [isLogin, setIsLogin] = useState(true);
+  if (!showRegister) return null;
 
-  // function onClick({ onClick }) {
-  //   return "hi";
-  // }
+  function handleChange(e) {
+    setCusInfo({
+      ...cusInfo,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function checkForm(e) {
+    e.preventDefault();
+
+    if (!e.target.value) {
+      setEmptyStatus({
+        ...cusInfo,
+        [e.target.name]: true,
+      });
+    }
+  }
 
   return (
-    <>
-      <Login isLogin={true} />
-      <Register />
-    </>
+    <div className="w-[304px]">
+      <h3 className="font-bold text-2xl">註冊帳號</h3>
+      <form>
+        <FormInput
+          content="Email"
+          name="email"
+          value={cusInfo.email}
+          onChange={handleChange}
+        />
+        {emptyStatus.email && (
+          <p className="text-warning text-sm font-bold">此欄位不可為空</p>
+        )}
+        <FormInput
+          content="您的暱稱"
+          name="name"
+          value={cusInfo.name}
+          onChange={handleChange}
+        />
+        {emptyStatus.name && (
+          <p className="text-warning text-sm font-bold">此欄位不可為空</p>
+        )}
+        <FormInput
+          content="密碼"
+          name="password"
+          value={cusInfo.password}
+          onChange={handleChange}
+        />
+        {emptyStatus.password && (
+          <p className="text-warning text-sm font-bold">此欄位不可為空</p>
+        )}
+        <FormInput
+          content="再次輸入密碼"
+          name="rePassword"
+          value={cusInfo.rePassword}
+          onChange={handleChange}
+        />
+        {emptyStatus.rePassword && (
+          <p className="text-warning text-sm font-bold">此欄位不可為空</p>
+        )}
+      </form>
+      <MainBtn value={"註冊帳號"} onClick={checkForm} />
+      <button className="block mx-auto mt-6 font-bold" onClick={onClickToLogin}>
+        登入
+      </button>
+    </div>
   );
 }
 
-// onClickTransferPage={onClick}
+export default function FormContent() {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isRegister, setIsRegister] = useState(false);
+
+  function onClickToRegister() {
+    setIsLogin(false);
+    setIsRegister(true);
+  }
+
+  function onClickToLogin() {
+    setIsLogin(true);
+    setIsRegister(false);
+  }
+
+  return (
+    <>
+      <Login showLogin={isLogin} onClickToRegister={onClickToRegister} />
+      <Register showRegister={isRegister} onClickToLogin={onClickToLogin} />
+    </>
+  );
+}
