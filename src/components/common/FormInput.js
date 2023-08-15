f083035e2dac0192a6a2cd0b7b1fcbed45a22481
Annie-Peng <djs910532@gmail.com>
2023-08-15 17:11:23 +0800

    feature-update input  errorMessage with empty value

--- src/components/common/FormInput.js
+++ src/components/common/FormInput.js
@@ -1,16 +1,23 @@
-export function Input({ name, value, showContent, onChange }) {
+export function Input({ name, value, placeholder, onChange }) {
   return (
     <input
       className="peer py-3 px-4 rounded-[10px] placeholder:text-tertiary text-medium w-full"
       name={name}
       value={value}
-      placeholder={`請輸入${showContent}`}
+      placeholder={placeholder}
       onChange={onChange}
     />
   );
 }
 
-export default function FormInput({ name, content, value, onChange, onBlur }) {
+export default function FormInput({
+  name,
+  content,
+  value,
+  onChange,
+  onBlur,
+  placeholder,
+}) {
   return (
     <label>
       <p className="mt-4 mb-1 text-sm font-bold">{content}</p>
@@ -19,6 +26,7 @@ export default function FormInput({ name, content, value, onChange, onBlur }) {
         value={value}
         showContent={content}
         onChange={onChange}
+        placeholder={placeholder}
       />
     </label>
   );
--- src/components/register/index.js
+++ src/components/register/index.js
@@ -6,58 +6,33 @@ import { postUser } from "../common/api.js";
 import { useForm, Controller } from "react-hook-form";
 
 export default function Register() {
-  const [emptyStatus, setEmptyStatus] = useState({
-    email: "",
-    name: "",
-    password: "",
-    rePassword: "",
-  });
-  const [nextCusInfo, setNextCusInfo] = useState({
-    email: "",
-    name: "",
-    password: "",
-    rePassword: "",
-  });
-
   const {
     handleSubmit,
     formState: { errors },
     control,
+    setError,
   } = useForm({
     defaultValues: {
       email: "",
+      name: "",
+      password: "",
+      rePassword: "",
     },
+    criteriaMode: "all",
   });
 
-  function handleChange(e) {
-    setNextCusInfo({
-      ...nextCusInfo,
-      [e.target.name]: e.target.value,
-    });
-  }
-
-  // function checkForm(e) {
-  //   e.preventDefault();
-
-  //   if (!e.target.value) {
-  //     setEmptyStatus({
-  //       ...nextCusInfo,
-  //       [e.target.name]: true,
+  // const onSubmit = async (data) => {
+  //   const response = await postUser(data);
+  //   const jsonData = await response.json();
+  //   console.log(jsonData);
+  //   if (response.status > 200) {
+  //     setError("errorType", {
+  //       type: response.status,
+  //       message: jsonData.message,
   //     });
   //   }
-  // }
-
-  // function handleClick(e) {
-  //   // e.preventDefault();
-  //   const obj = {
-  //     user: {
-  //       email: nextCusInfo.email,
-  //       nickname: nextCusInfo.name,
-  //       password: nextCusInfo.password,
-  //     },
-  //   };
-  //   action(obj);
-  // }
+  // };
+  // console.log(errors.errorType);
 
   return (
     <div className="w-[304px]">
@@ -71,10 +46,11 @@ export default function Register() {
             <>
               <FormInput
                 content="Email"
-                name={field.name}
+                name={field.email}
                 value={field.value}
                 onChange={field.onChange}
                 onBlur={field.onBlur}
+                placeholder="請輸入Email"
               />
               {errors.email && (
                 <p className="text-warning text-sm font-bold">此欄位不可為空</p>
@@ -82,34 +58,72 @@ export default function Register() {
             </>
           )}
         />
-
-        <FormInput
-          content="您的暱稱"
+        <Controller
+          control={control}
           name="name"
-          value={nextCusInfo.name}
-          onChange={handleChange}
+          rules={{ required: true }}
+          render={({ field }) => (
+            <>
+              <FormInput
+                content="您的暱稱"
+                name={field.name}
+                value={field.value}
+                onChange={field.onChange}
+                onBlur={field.onBlur}
+                placeholder="請輸入您的暱稱"
+              />
+              {errors.name && (
+                <p className="text-warning text-sm font-bold">此欄位不可為空</p>
+              )}
+            </>
+          )}
         />
-        {emptyStatus.name && (
-          <p className="text-warning text-sm font-bold">此欄位不可為空</p>
-        )}
-        <FormInput
-          content="密碼"
+        <Controller
+          control={control}
           name="password"
-          value={nextCusInfo.password}
-          onChange={handleChange}
+          rules={{
+            required: "此欄位不可為空",
+            minLength: {
+              value: 6,
+              message: "密碼不得少於6個字",
+            },
+          }}
+          render={({ field }) => (
+            <>
+              <FormInput
+                content="密碼"
+                name={field.password}
+                value={field.value}
+                onChange={field.onChange}
+                onBlur={field.onBlur}
+                placeholder="請輸入密碼"
+              />
+              {errors.password && (
+                <p className="text-warning text-sm font-bold">此欄位不可為空</p>
+              )}
+            </>
+          )}
         />
-        {emptyStatus.password && (
-          <p className="text-warning text-sm font-bold">此欄位不可為空</p>
-        )}
-        <FormInput
-          content="再次輸入密碼"
+        <Controller
+          control={control}
           name="rePassword"
-          value={nextCusInfo.rePassword}
-          onChange={handleChange}
+          rules={{ required: true }}
+          render={({ field }) => (
+            <>
+              <FormInput
+                content="再次輸入密碼"
+                name={field.rePassword}
+                value={field.value}
+                onChange={field.onChange}
+                onBlur={field.onBlur}
+                placeholder="請再次輸入密碼"
+              />
+              {errors.rePassword && (
+                <p className="text-warning text-sm font-bold">此欄位不可為空</p>
+              )}
+            </>
+          )}
         />
-        {emptyStatus.rePassword && (
-          <p className="text-warning text-sm font-bold">此欄位不可為空</p>
-        )}
         <MainBtn>註冊帳號</MainBtn>
       </Form>
       <button className="block mx-auto mt-6 font-bold">
